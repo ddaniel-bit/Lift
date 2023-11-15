1e9a762c3f72214cd74a89cf11e7190a8fc9073c
danko.daniel.janos <danko.daniel.janos@57-18>
2023-11-15 11:45:04 +0100

    Add project files.

---
+++ Lift/Liftezgetesek.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lift
+{
+    internal class Liftezgetesek
+    {
+        DateTime idopont;
+        int sorszam;
+        int indulo;
+        int erkezo;
+
+        public Liftezgetesek(DateTime idopont, int sorszam, int indulo, int erkezo)
+        {
+            this.idopont = idopont;
+            this.sorszam = sorszam;
+            this.indulo = indulo;
+            this.erkezo = erkezo;
+        }
+
+        public DateTime Idopont { get { return idopont; } }
+        public int Sorszam { get { return sorszam; } }
+        public int Indulo { get {  return indulo; } }
+        public int Erkezo { get {  return erkezo; } }
+    }
+}
---
+++ Lift/Program.cs
@@ -0,0 +1,70 @@
+﻿using Lift;
+using System.Collections;
+using System.Collections.Generic;
+
+internal class Program
+{
+    
+    private static void Main(string[] args)
+    {
+        List<Liftezgetesek> liftezgeteseklista = new List<Liftezgetesek>();
+        Console.WriteLine("code");
+        StreamReader sr = new StreamReader("lift.txt");
+        while (!sr.EndOfStream)
+        {
+            string[] mezok = sr.ReadLine().Split(" ");
+            liftezgeteseklista.Add(new Liftezgetesek(Convert.ToDateTime(mezok[0]), int.Parse(mezok[1]), int.Parse(mezok[2]), int.Parse(mezok[3])));
+        }
+
+        //3. feladat
+        Console.WriteLine($"3. feladat: Összes lifthasználat: {liftezgeteseklista.Count()}");
+        //4. feladat
+        Console.WriteLine($"4. feladat időszak: {liftezgeteseklista[0].Idopont} - {liftezgeteseklista[liftezgeteseklista.Count-1].Idopont}");
+        //5. feladat
+        Console.WriteLine($"5. feladat: Célszint max: {liftezgeteseklista.OrderBy(x => x.Erkezo).Last().Erkezo}");
+        //6-7. feladat
+        int kartyaszamimput;
+        int erkezoinput;
+        try
+        {
+            kartyaszamimput = Convert.ToInt32(Console.ReadLine());
+
+        }
+        catch (FormatException)
+        {
+            kartyaszamimput = 5;
+        }
+        try
+        {
+            erkezoinput = Convert.ToInt32(Console.ReadLine());
+        }
+        catch (FormatException)
+        {
+            erkezoinput = 5;
+        }
+        Console.WriteLine("6. feladat");
+        Console.WriteLine($"\tKártyaszám: {kartyaszamimput}");
+        Console.WriteLine($"\tCélszint: {erkezoinput}");
+        if (liftezgeteseklista.Any(x => x.Sorszam == kartyaszamimput && x.Erkezo == erkezoinput))
+        {
+            Console.WriteLine($"7. feladat: A(z) {kartyaszamimput}. kártyával utaztak a {erkezoinput}. emeletre!");
+        }
+        else
+        {
+            Console.WriteLine($"7. feladat: A(z) {kartyaszamimput}. kártyával nem utaztak a {erkezoinput}. emeletre!");
+        }
+        //8. feladat
+        Console.WriteLine("8. feladat: Statisztika");
+
+        try
+        {
+            liftezgeteseklista.GroupBy(x => x.Idopont).ToList().ForEach(x => Console.WriteLine($"\t{x.Key} - {x.Count()}x"));
+        }
+        catch (Exception)
+        {
+
+            throw new Exception("valami nem jo :(");
+        }
+
+    }
+}
